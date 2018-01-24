d090c607519c60eaec893d913e9da21936a9d9dc
Carlos Eduardo Marciano <carloseduardov8@hotmail.com>
2018-01-24 20:23:23 -0200

    Add lock button

---
+++ js/options.js
@@ -0,0 +1,34 @@
+
+// Hover animation time in milliseconds:
+var animTime = 400;
+
+
+$(document).ready(function () {
+
+	// Help button:
+	addHover("#help", "#help-hover", "#help-wrapper");
+
+	// Help button:
+	addHover("#lock", "#lock-hover", "#lock-wrapper");
+
+});
+
+
+function addHover(original, hover, wrapper){
+
+	// Hides the second object:
+	$(hover).hide();
+
+	// Adds a mouseenter event to wrapper:
+	$(wrapper).on("mouseenter", function(){
+		$(original).fadeOut(animTime);
+		$(hover).fadeIn(animTime);
+	});
+
+	// Adds a mouseleave event to wrapper:
+	$(wrapper).on("mouseleave", function(){
+		$(hover).fadeOut(animTime);
+		$(original).fadeIn(animTime);
+	});
+
+}
